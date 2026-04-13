55d2d108fb2c691b702fbe42942ab55610818d4d
bojanpaun <bojanpaun@gmail.com>
2026-04-13 23:09:56 +0200

    Fix AdminMenu CSS import

--- src/modules/menu/pages/AdminMenu.jsx
+++ src/modules/menu/pages/AdminMenu.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { supabase } from '../../../lib/supabase'
-import styles from './Admin.module.css'
+import styles from './AdminMenu.module.css'
 
 export default function Admin() {
   const navigate = useNavigate()
